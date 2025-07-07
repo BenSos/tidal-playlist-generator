aca905ea868763ba0c83f38ba7cc77635cdec752
Ben S <ben.sosinski@embodee.com>
2025-07-06 20:42:52 -0500

    fix: use 'export type' for type-only exports for isolatedModules compatibility

--- libs/perplexity-sdk/index.ts
+++ libs/perplexity-sdk/index.ts
@@ -40,7 +40,7 @@ export * from "./http/http";
 export * from "./auth/auth";
 export * from "./models/all";
 export { createConfiguration } from "./configuration"
-export { Configuration } from "./configuration"
+export type { Configuration } from "./configuration"
 export * from "./apis/exception";
 export * from "./servers";
 export { RequiredError } from "./apis/baseapi";
